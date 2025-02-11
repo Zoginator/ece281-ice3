7021cfda9e53ffbde665ecb33c0254bb8221f060
Zoginator <C27soren.herzog@afacademy.af.edu>
2025-02-11 14:43:42 -0700

    tests passed

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -47,8 +47,21 @@ begin
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with only Cin" severity failure;
+	       
+	   w_addends <= x"31"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"4" and w_Cout = '0') report "3+1 failed" severity failure;
+	   w_addends <= x"AC"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"6" and w_Cout = '1') report "10+12 failed" severity failure;
+	   w_addends <= x"D7"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"4" and w_Cout = '1') report "7+13 failed" severity failure;
+	   w_addends <= x"A5"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "10+5 failed" severity failure;
+	   w_addends <= x"A6"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "10+6 failed" severity failure;
 	
 		wait; -- wait forever
 	end process;	
